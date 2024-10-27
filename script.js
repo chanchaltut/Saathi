d49f7c12609e92f9a5a51e3fc0e8ccabe8d23c1d
chanchal <chanchalpradhan.coder@gmail.com>
2024-10-27 09:48:35 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// const hambugerMenu = document.querySelector(".hamburger-menu");
+// hambugerMenu.addEventListener("click", () => {
+//     header.classList.toggle("active");
+// });
+const header = document.querySelector("header");
+const hambugerMenu = document.querySelector(".hamburger-menu");
+
+hambugerMenu.addEventListener("click", () => {
+    header.classList.toggle("active");
+})
+
